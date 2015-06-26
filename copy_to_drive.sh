4ecf595212f5b38b7eda1eafe82525f7dd3f4c09
Johannes 'H1GHWAvE' Neutze <johannes.neutze@gmail.com>
2015-06-26 17:11:35 +0200

    second commit

---
+++ copy_to_drive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp Abschlussarbeit.pdf ~/Google\ Drive/Documents/University/2015ss/idp/
+cp chapters/Abstract.tex ~/Google\ Drive/Documents/University/2015ss/idp/Abstract.txt
+cp chapters/Conclusion.tex ~/Google\ Drive/Documents/University/2015ss/idp/Conclusion.txt
+cp chapters/Introduction.tex ~/Google\ Drive/Documents/University/2015ss/idp/Introduction.txz
+cp chapters/Methodology.tex ~/Google\ Drive/Documents/University/2015ss/idp/Methodology.txt
+cp chapters/Literature.tex ~/Google\ Drive/Documents/University/2015ss/idp/Literature.txt
+cp chapters/Results.tex ~/Google\ Drive/Documents/University/2015ss/idp/Results.txt
