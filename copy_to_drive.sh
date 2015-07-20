995a1c7d6bbb744cdb385942aa2f4deba798d360
Johannes 'H1GHWAvE' Neutze <johannes.neutze@gmail.com>
2015-07-20 21:06:54 +0200

    update

--- copy_to_drive.sh
+++ copy_to_drive.sh
@@ -6,3 +6,4 @@ cp chapters/Introduction.tex ~/Google\ Drive/Documents/University/2015ss/idp/Int
 cp chapters/Methodology.tex ~/Google\ Drive/Documents/University/2015ss/idp/Methodology.txt
 cp chapters/Literature.tex ~/Google\ Drive/Documents/University/2015ss/idp/Literature.txt
 cp chapters/Results.tex ~/Google\ Drive/Documents/University/2015ss/idp/Results.txt
+cp chapters/Appendix.tex ~/Google\ Drive/Documents/University/2015ss/idp/Appendix.txt	
